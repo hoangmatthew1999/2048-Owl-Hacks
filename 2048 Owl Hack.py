9b795a02da49608a064ae1d4b2488050e35f71e1
hoangmatthew1999 <hoangmatthew1999@gmail.com>
2017-11-10 18:03:41 -0800

    Add files via upload

---
+++ 2048 Owl Hack.py
@@ -0,0 +1,17 @@
+import random
+
+#class Shark:
+#    def __init__(self, name):
+#    sammy = Shark()
+
+#generates numbers randomly
+# ran_number = random.random()
+#print (ran_number)
+
+import pygame
+pygame.init()
+
+square = makeSprite("Square")
+
+
+
